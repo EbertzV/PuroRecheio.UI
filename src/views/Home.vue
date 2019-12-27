f94162507681b35151780142e455e5b6b9bc1e23
EbertzV <ebertzv@outlook.com>
2019-12-26 23:55:32 -0300

    Teste com classes do vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,14 @@
 <template>
     <div class="home">
-        <h1>Homepage</h1>
+        <h1 class="display-4">Homepage</h1>
+        <h4 class="display-1">Teste</h4>
+        <p class="headline">Teste</p>
+        <p class="subheading">Teste</p>
+        <p class="caption">Teste</p>
+        <p class="red">Teste</p>
+        <p class="red white--text darken-3 text--darken-2">Teste</p>
+        
+
     </div>
 </template>
 
