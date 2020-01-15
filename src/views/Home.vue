dfd74497a65ba197fbc0b10949bcdbc1bde1fcaf
EbertzV <ebertzv@outlook.com>
2020-01-15 00:55:00 -0300

    Melhoria no banner inicial

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,10 +3,13 @@
         <v-card pl-2 class="elevation-12">
             <v-layout text-xs-center wrap>
                 <v-flex xs12 sm4 md4>
-                    <v-img xs2 sm2 md2 :src="require('../assets/logo_transparent.png')" class="my-3" height="200"></v-img>
+                    <v-img xs2 sm2 md2 :src="require('../assets/logo_transparent.png')" class="my-3" height="480"></v-img>
                 </v-flex>
-                <v-flex align-self-center>
+                <v-flex xs12 sm4 md4 align-self-center>
                     <h1 class="display-4">Puro Recheio</h1>
+                </v-flex>
+                <v-flex>
+
                 </v-flex>
             </v-layout>
         </v-card>   
