89ce98bd411d9e5ef67f9f9faafb7ca81e36fb63
EbertzV <ebertzv@outlook.com>
2020-01-15 22:45:17 -0300

    Criação da toolbar, imagem de conteúdo da página inicial e ajustes na barra de título

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,35 @@
 <template>
-    <v-container>
-        <v-card pl-2 class="elevation-12">
-            <v-layout text-xs-center wrap>
-                <v-flex xs12 sm4 md4>
-                    <v-img xs2 sm2 md2 :src="require('../assets/logo_transparent.png')" class="my-3" height="480"></v-img>
-                </v-flex>
-                <v-flex xs12 sm4 md4 align-self-center>
-                    <h1 class="display-4">Puro Recheio</h1>
-                </v-flex>
-                <v-flex>
+    <v-card pl-2 class="elevation-10">
+        <v-layout text-xs-center wrap>
+            <v-flex xs12 sm4 md4>
+                <v-img xs2 sm2 md2 :src="require('../assets/logo_transparent.png')" class="my-3" contain height="250"></v-img>
+            </v-flex>
+            <v-flex xs12 sm4 md4 align-self-center>
+            <h1 class="display-4" >Puro Recheio</h1>
+            </v-flex>
+            <v-flex>
+        
+            </v-flex>
+        </v-layout>
 
-                </v-flex>
-            </v-layout>
-        </v-card>   
-    </v-container>
-    
+        <v-layout>
+            <v-flex>
+                <v-toolbar>
+                    <v-btn>Página inicial</v-btn>
+                    <v-btn>Conheça nossos produtos</v-btn>
+                    <v-btn>Faça seu pedido</v-btn>
+                    <v-btn>Contate-nos</v-btn>
+                </v-toolbar>
+            </v-flex>
+        </v-layout>
+
+        <v-layout>
+            <v-flex>
+                <v-img :src="require('../assets/facebook_cover_photo_2.png')"></v-img>
+            </v-flex>
+        </v-layout>
+    </v-card> 
+
 </template>
 
 <script>
